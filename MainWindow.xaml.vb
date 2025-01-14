34593897a087cec25bf41c92e458c0113e2ff476
jewfu123 <jewfu_123@163.com>
2025-01-15 05:40:06 +0900

    添加项目文件。

---
+++ MainWindow.xaml.vb
@@ -0,0 +1,6 @@
+﻿Class MainWindow
+    Private Sub btn_hello_Click(sender As Object, e As RoutedEventArgs) Handles btn_hello.Click
+        MsgBox("sb sb hurry come down!")
+
+    End Sub
+End Class
