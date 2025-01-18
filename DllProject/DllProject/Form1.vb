babc472543484e672687055e6e521a55b33b531b
jewfu123 <jewfu_123@163.com>
2025-01-18 16:51:42 +0900

    Dll Sample

---
+++ DllProject/DllProject/Form1.vb
@@ -0,0 +1,22 @@
+﻿Imports GFXII.GDI
+
+Public Class Form1
+    Private ImgMan As New ImageManager
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles Me.Load
+        ImgMan.LoadImage("Sample", "Sample.jpg")
+        ImgMan.LoadImage("Sample2", "Sample2.png")
+    End Sub
+
+    Private Sub Form1_Paint(sender As Object, e As PaintEventArgs) Handles Me.Paint
+        ImgMan.DrawImage(Me, "Sample", New Point(0, 0), New Point(Me.Width, Me.Height))
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        ImgMan.DrawImage(PictureBox1, "Sample2", New Point(0, 0), New Point(PictureBox1.Width, PictureBox1.Height))
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        ImgMan.RemoveImage("Sample2")
+        PictureBox1.Refresh()
+    End Sub
+End Class
---
+++ DllProject/GFXII/ImageManager.vb
@@ -0,0 +1,40 @@
+﻿Imports System.Drawing
+
+Namespace GDI
+    Public Class ImageManager
+        Public Images As New Dictionary(Of String, Image)
+        Public Sub LoadImage(Name As String, Path As String)
+            If Images.ContainsKey(Name) Then
+                Exit Sub
+            Else
+                Try
+                    Dim i As Image = Image.FromFile(Path)
+                    Images.Add(Name, i)
+                Catch ex As Exception
+
+                End Try
+            End If
+        End Sub
+
+        Public Sub RemoveImage(Name As String)
+            If Images.ContainsKey(Name) Then Images.Remove(Name)
+        End Sub
+
+        Public Sub DrawImage(Surface As Object, Name As String, Position As Point, Optional Size As Point = Nothing)
+            Try
+                If Images.ContainsKey(Name) Then
+                    Dim G As Graphics = Surface.CreateGraphics
+                    If Size.IsEmpty Then
+                        G.DrawImage(Images(Name), Position)
+                    Else
+                        G.DrawImage(Images(Name), New Rectangle(Position.X, Position.Y, Size.X, Size.Y))
+
+                    End If
+                End If
+            Catch ex As Exception
+
+            End Try
+        End Sub
+    End Class
+End Namespace
+
